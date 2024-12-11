de75f7b0174280b982a91d0a2744f2634ba4d73d
Omar Eloui <contact@omareloui.com>
2024-12-11 12:11:14 +0200

    feat(auth): log when the access token expires

--- src/app.mjs
+++ src/app.mjs
@@ -1,13 +1,22 @@
 import "dotenv/config";
+import fs from "node:fs/promises";
 import { signin } from "./auth.mjs";
 import { getReferrals } from "./referrals.mjs";
 import { quitDriver } from "./driver.mjs";
+import { CACHE_DIR } from "./consts.mjs";
+import { getTimestamp } from "./utils/time.mjs";
 
 async function init() {
   try {
     const tokens = await signin();
     const referrals = await getReferrals(tokens.accessToken);
-    console.log({ referrals });
+    if (!referrals) {
+      const s = await fs.stat(`${CACHE_DIR}/tokens.cache`);
+      await fs.writeFile(
+        `${CACHE_DIR}/__${getTimestamp()}_access_failed.cache`,
+        `failed on ${new Date()}, the tokens was generated at ${s.mtime},`,
+      );
+    }
   } finally {
     await quitDriver();
   }
