56c7b4e270ab870d444c163e87db956b75024e11
Omar Eloui <contact@omareloui.com>
2024-12-23 21:20:53 +0200

    feat(sms): add the ability to send an sms

--- src/config.mjs
+++ src/config.mjs
@@ -10,17 +10,27 @@ const ConfigZ = z.object({
   }),
   broadcastEmails: z.array(z.string().email()),
   broadcastPhoneNumbers: z.array(z.string()),
+  twilio: z.object({
+    accountSid: z.string().min(3),
+    authToken: z.string().min(3),
+    fromNumber: z.string().min(3),
+  }),
 });
 
 const config = ConfigZ.parse({
   dbURI: process.env.DATABASE_URL,
   broadcastEmails: (process.env.EMAILS_TO_NOTIFY || "").split(","),
-  broadcastPhoneNumbers: (process.env.PHONE_NUMBERS_TO_NOTIFY || "").split(","),
   smtp: {
     auth: {
       email: process.env.SMTP_AUTH_EMAIL,
       password: process.env.SMTP_AUTH_PASSWORD,
     },
+  },
+  broadcastPhoneNumbers: (process.env.PHONE_NUMBERS_TO_NOTIFY || "").split(","),
+  twilio: {
+    accountSid: process.env.TWILIO_ACCOUNT_SID,
+    authToken: process.env.TWILIO_AUTH_TOKEN,
+    fromNumber: process.env.TWILIO_FROM_NUMBER,
   },
 });
 
